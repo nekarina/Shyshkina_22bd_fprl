cab9d3c595e70e401f4ed62f56773b5aef8b2281
Шишкіна Каріна <ks6611788@gmail.com>
2024-09-27 17:20:58 +0300

    2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,15 @@
 import math
-def point(x, y, z, x0, y0, z0, R):
-    d = math.sqrt((x-x0)**2 + (y - y0)**2 + (z - z0)**2)
-    return d < R
-x, y, z = 1, 2, 3
-x0, y0, z0 = 0, 0, 0
-R = 5
-inside=point(x, y, z, x0, y0, z0, R)
-print(f"Точка всередині сфери: {inside}")
+x = float(input("Enter x: "))
+y = float(input("Enter y: "))
+z = float(input("Enter z: "))
+x0 = float(input("Enter x0: "))
+y0 = float(input("Enter y0: "))
+z0 = float(input("Enter z0: "))
+R = float(input("Enter R: "))
+d = math.sqrt((x-x0)**2 + (y - y0)**2 + (z - z0)**2)
+if d < R:
+    print("Точка знаходиться всередині сфери")
+elif d==R:
+    print("Точка лежить на поверхні сфери")
+else :
+    print("Точка знаходиться поза поверхнею сфери")
